a392fe8664469fbcbd37e63cabacfc903c2d914f
Dave Hawkins <dhawkins01@gmail.com>
2019-06-23 14:27:27 -0400

    testing bands in town api

--- liri.js
+++ liri.js
@@ -41,9 +41,36 @@ function concertThis() {
         }
     }
     // build the url for the bands in town api for axios
-    queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     // console.log(queryURL);
+
+    // start our Axios call
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log("---------------Data---------------");
+              console.log(error.response.data);
+              console.log("---------------Status---------------");
+              console.log(error.response.status);
+              console.log("---------------Status---------------");
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an object that comes back with details pertaining to the error that occurred.
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          });
     
+
 
 }
 
