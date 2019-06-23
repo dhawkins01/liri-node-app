afd4f8e7fb1f0c948530b76c39922ccfd3b0bc6b
Dave Hawkins <dhawkins01@gmail.com>
2019-06-23 11:55:14 -0400

    added more psuedo code

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+// Liri application project
+// You can find my current portfolio at www.dhawkinsjr.com
+
+
 require("dotenv").config();
 
 // import the keys.js file
@@ -14,3 +18,41 @@ var spotify = new Spotify(keys.spotify);
 
 // make a variable to hold the command
 var command = process.argv[2];
+
+// Here we will create a function to fun each command
+//////////////////////////////////////////////////////
+
+function concertThis() {
+    // code for concertThis functions goes here
+}
+
+function spotifyThisSong() {
+    // code for spotifyThisSong goes here
+}
+
+function movieThis() {
+    // code for movieThis goes here
+}
+
+function doWhatItSays() {
+    // code for doWhatItSays goes here
+}
+
+// Here we will put a switch statement that will run each function based on the command
+switch(command) {
+    case "concert-this":
+        concertThis();
+        break;
+    
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
