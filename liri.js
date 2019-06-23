2ae866d06adc978765d5d5638b633ccab1a712f1
Dave Hawkins <dhawkins01@gmail.com>
2019-06-23 11:20:28 -0400

    added some psuedo code for liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
 require("dotenv").config();
+
+// import the keys.js file
+var keys = require("./keys.js");
+
+// access the keys inmormation for spotify
+var spotify = new Spotify(keys.spotify);
+
+// liri.js needs to be able to hold the following commands:
+// - concert-this
+// - spotify-this-song
+// - movie-this
+// - do-what-it-says
+
+// make a variable to hold the command
+var command = process.argv[2];
