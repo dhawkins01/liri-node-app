c737f8095db308717a86496587fe8390456a2315
Dave Hawkins <dhawkins01@gmail.com>
2019-06-23 14:11:01 -0400

    added functionality... if no movie is enterend in movie-this, default to mr. nobody

--- liri.js
+++ liri.js
@@ -27,7 +27,7 @@ var command = process.argv[2];
 
 function concertThis() {
     // code for concertThis functions goes here
-    
+
 }
 
 function spotifyThisSong() {
@@ -40,46 +40,51 @@ function movieThis() {
     // create an empty variable for holding the movie name
     var movieName = "";
 
-    // create a for loop to handle movie titles with more than one word
-    for (i = 3; i < process.argv.length; i++) {
-        movieName += process.argv[i] + "+";
+    // if no movie name is entered, default to Mr Nobody
+    if (process.argv[3] === undefined) {
+        movieName = "Mr.Nobody";
+    } // else build the movie name
+    else {
+        // create a for loop to handle movie titles with more than one word
+        for (i = 3; i < process.argv.length; i++) {
+            movieName += process.argv[i] + "+";
+        }
     }
-    
     // build the url to access OMDB api with Axios
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
     // use axios to pull the data from the omdb api
     axios.get(queryUrl).then(
-        function(response) {
-          console.log("**********Movie-This**********");
-          console.log("Title: " + response.data.Title);
-          console.log("Year: " + response.data.Year);
-          console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-          console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-          console.log("Country Produced: " + response.data.Country);
-          console.log("Language: " + response.data.Language);
-          console.log("Plot: " + response.data.Plot);
-          console.log("Actors: " + response.data.Actors);
+        function (response) {
+            console.log("**********Movie-This**********");
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country Produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
         })
-        .catch(function(error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
         });
 }
 
@@ -88,11 +93,11 @@ function doWhatItSays() {
 }
 
 // Here we will put a switch statement that will run each function based on the command
-switch(command) {
+switch (command) {
     case "concert-this":
         concertThis();
         break;
-    
+
     case "spotify-this-song":
         spotifyThisSong();
         break;
